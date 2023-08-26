742a2e6e0eadd1daea1b0beb308f4f8abf014661
BitPingu <gavin.eugenio@gmail.com>
2023-08-26 16:53:50 -0400

    Added website color scheme and experience section, navbar revamp

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 $(function() {
-
-    // Check for dark mode
-    var currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-    if (currentTheme) {
-        document.documentElement.setAttribute('data-theme', currentTheme);
-        if (currentTheme === 'dark') {
-            $('.switch').trigger('click')
-        }
+    
+    // Navbar color
+    var $nav = $('.navbar')
+    if ($(this).scrollTop() > $nav.height() - 120) {
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height() - 120);
     }
+    $(document).scroll(function() {
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height() - 120);
+    });
 
-    // Dark mode switch
-    $('.switch').on('click', function(e) {
-        if (e.target.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        }
+    // Home button
+    $('#home_button').on('click', function() {
+        window.open('/', '_self')
+    });
+    $('#404_button').on('click', function() {
+        window.open('/', '_self')
+    })
+
+    // Social buttons
+    $('#email').on('click', function() {
+        window.open('mailto:gavin.eugenio@gmail.com', '_blank')
+    });
+    $('#email_m').on('click', function() {
+        window.open('mailto:gavin.eugenio@gmail.com', '_blank')
+    });
+    $('#github').on('click', function() {
+        window.open('https://github.com/BitPingu', '_blank')
+    });
+    $('#github_m').on('click', function() {
+        window.open('https://github.com/BitPingu', '_blank')
+    });
+    $('#linkedin').on('click', function() {
+        window.open('https://www.linkedin.com/in/gavin-eugenio', '_blank')
+    });
+    $('#linkedin_m').on('click', function() {
+        window.open('https://www.linkedin.com/in/gavin-eugenio', '_blank')
     });
 
     // Mobile navbar
-    $('#socials_button').on('click', function(e) {
-        var links = document.getElementById('mobile_links');
+    var links = document.getElementById('mobile_links');
+    $('#socials_dropdown').on('click', function() {
         if (links.style.display === 'block') {
             links.style.display = 'none';
         } else {
             links.style.display = 'block'
         }
+    });
+    $(document).scroll(function() {
+        links.style.display = 'none';
     });
 
     // Hi
